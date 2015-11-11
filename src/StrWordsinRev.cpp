41e330610fe54aac74aeed40e42b4aa432c4aea7
ashuthosh-heda <ashuthosh.heda@gmail.com>
2015-11-11 19:48:35 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -9,9 +9,33 @@ OUTPUT: Modify the string according to the logic.
 
 NOTES: Don't create new string.
 */
-#include <Stdio.h>
-#include <string.h>
+#include <stdio.h>
+
+void revStr1(char* str, int len){
+	int st = 0;
+	int en = len - 1;
+	int temp;
+
+	while (st < en){
+		temp = str[st];
+		str[st] = str[en];
+		str[en] = temp;
+		st++;
+		en--;
+	}
+}
 
 void str_words_in_rev(char *input, int len){
+	revStr1(input, len);
 	
+	int i = 0;
+	int t = 0;
+
+	for (i = 0; i < len; i++){
+		if (input[i] == ' '){
+			revStr1(&input[t], i - t);
+			t = i + 1;
+		}
+	}
+	revStr1(&input[t], i - t);
 }
